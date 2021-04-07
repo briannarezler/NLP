c8386d15317602b92fa44ee1208b382ff3b2150a
briannarezler <brianna_rezler1@baylor.edu>
2021-04-07 12:48:05 -0500

    worked on homework

--- NLP_exercise.py
+++ NLP_exercise.py
@@ -1,7 +1,7 @@
 # Create a wordcloud of the top 15 words that are the most recurring.
 # Make sure to remove all stop words. Use only noun words for this analysis.
 from textblob import TextBlob
-import nltk
+from nltk import pos_tag, word_tokenize
 
 from nltk.corpus import stopwords
 from pathlib import Path
@@ -12,28 +12,37 @@ blob = TextBlob(Path("book of John text.txt").read_text())
 
 stops = stopwords.words("english")
 
-items = blob.word_counts.items()
-# print(items)
+more_stop_words = ["thy", "ye", "verily", "thee", "hath", "thou", "say", "art", "shall"]
+stops += more_stop_words
 
-items = [item for item in items if item[0] not in stops]
+noun_phrases = blob.noun_phrases
+
+items = blob.word_counts.items()
+
+items = [item for item in items if item[0] not in stops if item[0] in noun_phrases]
+
 
 from operator import itemgetter
 
 sorted_items = sorted(items, key=itemgetter(1), reverse=True)
 
-top15 = sorted_items[1:16]
+top15 = list(sorted_items[0:15])
+# print(top15)
 
+my_words = ""
+for p in top15:
+    my_words += p[0] + " "
+print(my_words)
 
 from wordcloud import WordCloud
 import imageio
 import matplotlib.pyplot as plt
 
-text = Path("book of John text.txt").read_text()
-# print(text)
+wordcloud = WordCloud(
+    stopwords=stops, colormap="Blues", background_color="gray", max_words=15
+)
 
-wordcloud = WordCloud(colormap="prism", background_color="white", max_words=15)
-
-wordcloud = wordcloud.generate(text)
+wordcloud = wordcloud.generate(my_words)
 
 wordcloud = wordcloud.to_file("BookOfJohnCloud.png")
 
